f37b44b48556dc365774d645f053ffdc346d828c
kth0319 <100022579+kth0319@users.noreply.github.com>
2022-03-03 21:47:53 +0900

    Delete MainSite directory

--- MainSite/JS/signup.js
+++
@@ -1,9 +0,0 @@
-function dosubmit(e){
-    const id = document.querySelector('#id').value
-    const pwd = document.querySelector('#pwd').value
-    localStorage.setItem(id, pwd)
-    e.preventDefault()
-    window.location.href = 'login.html'
-}
-
-document.addEventListener('submit', dosubmit) // form submit 후 login page로 이동
